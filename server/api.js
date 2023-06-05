7f4d8755a26a4a444ad757968b3706a8d72f8f02
Sveta Kryukova <kryukova.sv.13@gmail.com>
2023-06-05 14:39:21 +0300

    prettifying2

--- server/api.js
+++ server/api.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const client = require('./placesdb.js');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(cors());
 app.listen(PORT, () => {
