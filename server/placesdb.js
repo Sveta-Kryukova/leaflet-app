86dd2afee0040886d3be2b3973c4f1fd231e5bd6
Sveta Kryukova <kryukova.sv.13@gmail.com>
2023-06-05 15:00:36 +0300

    refresh placesdb

--- server/placesdb.js
+++ server/placesdb.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const { Client } = require('pg')
 const client = new Client({
-  host: 'localhost',
-  user: 'postgres',
-  port: 5432,
-  password: '666sv666',
-  database: 'postgres'
-})
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+});
  module.exports = client
